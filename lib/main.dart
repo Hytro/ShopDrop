0e80a833855d059d42be58b98c97e34b9678a8f5
Marius Hagelin <marius.hagelin@gmail.com>
2022-01-24 13:20:52 +0100

    Initial Data & Grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -12,21 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Shop Drop'),
-      ),
-      body: Center(
-        child: Text('Shop Shop Shop Consume Consume Consume'),
-      ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
